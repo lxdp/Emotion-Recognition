fca13f11e8ec05ee8b97fd408f14c8a682f4e50e
Lucas XD Pedersen <lucasxdpedersen@Larss-MacBook-Air-8.local>
2024-09-20 14:13:10 +0100

    Improvements made to the model

--- EmotionRecognitionPy/main.py
+++ EmotionRecognitionPy/main.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 from keras.src.layers.convolutional.conv2d import Conv2D
-from keras.layers import MaxPooling2D
+from keras.layers import MaxPooling2D, Input
 from keras.src.models.sequential import Sequential
 from keras.src.layers.reshaping.flatten import Flatten
 from keras.src.layers.core.dense import Dense
@@ -19,6 +19,12 @@ import time
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
 from scipy.signal._wavelets import cascade
 from pickle import NONE
+from keras.src.applications.resnet import ResNet50
+from keras.src.layers.pooling.global_average_pooling2d import GlobalAveragePooling2D
+from keras.src.models.model import Model
+from sklearn.utils.class_weight import compute_class_weight
+from keras.regularizers import l2
+from sklearn.utils import class_weight
 
 model = load_model('emotionRecognitionModel.keras')
 
@@ -40,11 +46,11 @@ if not camera.isOpened():
 def processFace(face):
     greyFrame = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
     faceDetection = cascadeFace.detectMultiScale(greyFrame, scaleFactor = 1.1, minNeighbors=5, minSize=(30,30))
-    
+
     if len(faceDetection) == 0:
         print("No face detected")
         return None
-    
+
     (x, y, w, h) = faceDetection[0]
     faces = face[y:y+h, x:x+w]
     greyscaleFace = cv2.cvtColor(faces, cv2.COLOR_BGR2GRAY)
@@ -74,12 +80,12 @@ while True:
         break
 
     imageArray = processFace(frame)
-    
+
     if imageArray is not None:
         index = prediction(model, imageArray)
-    
+
         emotion = emotions.get(index[0], 'unclear')
-    
+
         cv2.putText(frame, f'Emotion: {emotion}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow('Camera', frame)
@@ -125,30 +131,33 @@ imageList = np.expand_dims(imageList, axis=-1)
 trainImageList, imageListValidation, trainEmotionList, emotionListValidation = train_test_split(imageList, emotionListHotEncode, test_size=0.2, random_state=42)
 
 
+class_weights = compute_class_weight(class_weight='balanced', classes=np.unique(trainEmotionList), y = trainEmotionList.ravel())
+class_weights_dict = dict(enumerate(class_weights))
+
 model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 1)),
+    Conv2D(64, (3, 3), activation='relu', input_shape=(48, 48, 1), kernel_regularizer=l2(0.001)),
     BatchNormalization(),
     MaxPooling2D((2,2)),
-    Conv2D(64, (3, 3), activation='relu'),
+    Conv2D(128, (3, 3), activation='relu', kernel_regularizer=l2(0.001)),
     BatchNormalization(),
     MaxPooling2D((2, 2)),
-    Conv2D(128, (3, 3), activation='relu'),
+    Conv2D(256, (3, 3), activation='relu', kernel_regularizer=l2(0.001)),
     BatchNormalization(),
     MaxPooling2D((2, 2)),
-    Conv2D(128, (3, 3), activation='relu'),
+    Conv2D(256, (3, 3), activation='relu', kernel_regularizer=l2(0.001)),
     BatchNormalization(),
     Flatten(),
-    Dense(128, activation='relu'),
+    Dense(256, activation='relu', kernel_regularizer=l2(0.001)),
     Dropout(0.5),
     Dense(len(labelEncoder.classes_), activation='softmax')
 ])
 
 dataaug = ImageDataGenerator(
-    rotation_range=10,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
+    rotation_range=5,
+    width_shift_range=0.06,
+    height_shift_range=0.06,
+    shear_range=0.06,
+    zoom_range=0.06,
     horizontal_flip=True,
     fill_mode='nearest'
     
@@ -161,10 +170,12 @@ model.compile(optimizer=Adam(learning_rate=0.0001),
 earlyStop = EarlyStopping(monitor='val_accuracy', patience=5, restore_best_weights=True)
 
 history = model.fit(dataaug.flow(trainImageList, trainEmotionList, batch_size=16),
-                    epochs=40, 
+                    epochs=60, 
                     validation_data=(imageListValidation, emotionListValidation),
+                    class_weight=class_weights_dict,
                     callbacks=[earlyStop],
                     verbose=1)
+              
 
 loss, accuracy = model.evaluate(trainImageList, trainEmotionList, verbose=1)
 print(f'Validation accuracy: {accuracy:.4f}')
